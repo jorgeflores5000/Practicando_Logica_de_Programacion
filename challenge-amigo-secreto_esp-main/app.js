25662261bda8a41e0ac237c57a83c868d84abf86
Jorge Flores <jorgeflo5000@gmail.com>
2025-02-10 14:19:23 -0600

    funcion agregar nombres

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,32 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombresAmigos = [];
+
+
+function agregarNombre(){
+    nombresAmigos.push(document.getElementById("amigo").value);
+    console.log(nombresAmigos);
+    limpiarCaja();
+}
+
+function validarEntrada(){
+    if(!document.getElementById("amigo").value){
+        alert("Debes ingresar un nombre");
+    } else {
+        agregarNombre();
+    }
+}
+
+function actualizarLista(){
+    
+}
+
+function visualizarLista(){
+
+}
+
+function sortearAmigos(){
+
+}
+ function limpiarCaja(){
+    document.getElementById("amigo").value = "";
+}
