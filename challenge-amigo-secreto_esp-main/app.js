2d5ddc4061f5c330b78c68983d25af8ff9384e2b
Jorge Flores <jorgeflo5000@gmail.com>
2025-02-10 21:47:02 -0600

    Amigo secreto terminado

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -2,31 +2,55 @@
 let nombresAmigos = [];
 
 
-function agregarNombre(){
+function agregarAmigo(){
     nombresAmigos.push(document.getElementById("amigo").value);
     console.log(nombresAmigos);
     limpiarCaja();
+    actualizarLista();
 }
 
 function validarEntrada(){
     if(!document.getElementById("amigo").value){
-        alert("Debes ingresar un nombre");
+        alert("Por favor ingrese un nombre");
     } else {
-        agregarNombre();
+        agregarAmigo();
     }
 }
 
 function actualizarLista(){
+    let = limpiarlista = document.getElementById("listaAmigos");
+    limpiarlista.innerHTML = "";
     
+    if(nombresAmigos.length != 0){
+        for(let i = 0; i < nombresAmigos.length; i++){
+            let lista = document.getElementById("listaAmigos");
+            let elemento = document.createElement("li");
+            elemento.textContent = nombresAmigos[i];
+            lista.appendChild(elemento);
+        }
+    } else {
+        validarEntrada();
+    }
 }
 
-function visualizarLista(){
 
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
 }
 
-function sortearAmigos(){
+function sortearAmigo(){
 
+    if(nombresAmigos.length != 0){
+        let amigoSecreto = Math.floor(Math.random()*nombresAmigos.length);
+        let amigoSorteado = nombresAmigos[amigoSecreto];
+        asignarTextoElemento('ul',`El amigo secreto es: ${amigoSorteado}`);
+    } else {
+        alert("La lista de amigos está vacía");
+    }
 }
  function limpiarCaja(){
     document.getElementById("amigo").value = "";
 }
+
